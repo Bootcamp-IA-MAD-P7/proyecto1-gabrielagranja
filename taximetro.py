e4510dcc5180c09a579c57a370d0b80fa1426569
gabrielagranja-ux <gabriela.granja@gmail.com>
2026-05-08 15:08:48 +0200

    Corrigiendo identación

--- taximetro.py
+++ taximetro.py
@@ -15,7 +15,7 @@ def taximeter():
     """
     Función para mostrar las opciones del taxímetro.
     """
-    print ("Bienvenido")
+    print ("Hola")
     print ("Escoge una de las siguientes opciones: 'start', 'stop', 'move', 'finish','exit'\n")
     
     # Declaración de variables para contar el tiempo detenido y en movimiento
@@ -49,50 +49,60 @@ def taximeter():
             tiempo_movimiento = 0
             estado_actual = "stopped" #Inciamos en estado detenido
             inicio_estado = time.time()
+
             print("Viaje iniciado. EStado inciial: 'stopped'")
 
         elif command == ("stop", "move"):
             if not viaje_activo:
                 print("Error: no hay un viaje en curso. Por favor empie un viaje con 'start'.")
                 continue
-                #calcula el tiempo transcurrido en el estado actual antes de cambiar
-                duration= time.time() - inicio_estado
-                if estado_actual == "stopped":
+
+            #calcula el tiempo transcurrido en el estado actual antes de cambiar
+            
+            duration= time.time() - inicio_estado
+                
+            if estado_actual == "stopped":
                     tiempo_parado += duration
-                else:
+            else:
                     tiempo_movimiento += duration
 
-                # Cambo el estado actual y reinicia el tiempo de inicio del estado
-                    estado_actual == "stopped" if command == "stop" else "moving"
-                    inicio_estado = time.time()
-                    print(f"State changed to '{state} .")
+            # Cambo el estado actual y reinicia el tiempo de inicio del estado
+            estado_actual == "stopped" if command == "stop" else "moving"
+            inicio_estado = time.time()
+            
+            print(f"state changed to '{state} .")
         
         elif command == "finish":   
-              if not viaje_activo:
+            if not viaje_activo:
                 print("Error: no hay un viaje en curso para finalizar.")
                 continue
               
             # Agrega tiempo del último estado
-                duration = time.time() - inicio_estado
-                if estado_actual == "stopped":
-                    tiempo_parado += duration
-                else:
-                    tiempo_movimiento += duration
+            
+            duration = time.time() - inicio_estado
+      
+            if estado_actual == "stopped":
+                tiempo_parado += duration
+            else:
+                tiempo_movimiento += duration
 
-                # Calcula la tarifa total y muestra el reumen del viaje
-                total_fare = calculate_fare(tiempo_parado, tiempo_movimiento)
-                print(f"\n--- Trip Summary ---")
-                print(f"Tiempo parado: {tiempo_parado:.1f} s")
-                print(f"Tiempo en movimiento: {tiempo_movimiento:.1f} s")
-                print(f"total_fare: € {total_fare:.2f}")
-                print("-------------------------------\n")
+            # Calcula la tarifa total y muestra el reumen del viaje
+            
+            total_fare = calculate_fare(tiempo_parado, tiempo_movimiento)
+            print("\n--- Resumen de tu Viaje ---")
+            print(f"Tiempo parado: {tiempo_parado:.1f} s")
+            print(f"Tiempo en movimiento: {tiempo_movimiento:.1f} s")
+            print(f"total_fare: € {total_fare:.2f}")
+            print("-------------------------------\n")
 
-                #Reset la svariables para el próximo viaje 
-                trip_active = False
-                estado_actual = None        
+            #Reset la svariables para el próximo viaje 
+            trip_active = False
+            estado_actual = None        
+        
         elif command == "exit":
             print("Saliendo del taxímetro. ¡Hasta luego!")
             break   
+        
         else:
             print("Comando no reconocido. Por favor, use 'start', 'stop', 'move', 'finish' o 'exit'.")                                
                                        
